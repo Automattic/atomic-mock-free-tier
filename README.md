f5d634947d3f712d2a097a2edec49d4520787455
Derek Smart <smart@automattic.com>
2021-03-25 17:32:39 -0400

    Initial commit
    Matches the caps found in wpcomsh
    
    Update README.md
    
    Update README.md

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # atomic-mock-free-tier
-Mocks user capabilities to match those on a Free tier of Atomic. 
+Mocks user capabilities to match those on a Free tier of Atomic, which are defined in wpcomsh in `map_atomic_plan_cap`
---
+++ atomic-mock-free-tier.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Plugin Name: Mock Free Atomic
+ * Description: Will match user capabilities for those on a Free tier of Atomic.
+ */
+
+add_filter( 'map_meta_cap', 'mock_woa_free_plan_caps', 11, 2 );
+function mock_woa_free_plan_caps( $caps, $cap ) {
+	$theme_caps = [
+		'edit_themes',
+		'switch_themes',
+		'install_themes',
+		'update_themes',
+		'delete_themes',
+		'upload_themes',
+	];
+
+	$plugin_caps = [
+		'activate_plugins',
+		'install_plugins',
+		'edit_plugins',
+		'upload_plugins',
+	];
+
+	$all_atomic_caps = array_merge( $theme_caps, $plugin_caps );
+
+	if ( in_array( $cap, $all_atomic_caps, true ) ) {
+		$caps[] = 'do_not_allow';
+  }
+
+	return $caps;
+}
