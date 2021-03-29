1e9626accb3b0c919af03a4ee8db417cc6c96cb8
Derek Smart <smart@automattic.com>
2021-03-29 17:38:11 -0400

    Remove core update things

--- atomic-mock-free-tier.php
+++ atomic-mock-free-tier.php
@@ -6,12 +6,16 @@
 
 add_filter( 'map_meta_cap', 'mock_woa_free_plan_caps', 11, 2 );
 function mock_woa_free_plan_caps( $caps, $cap ) {
+	// Else the site is a free Atomic site
+	// so we need to disable atomic features caps.
 	$theme_caps = [
 		'edit_themes',
+		'switch_themes',
 		'install_themes',
 		'update_themes',
 		'delete_themes',
 		'upload_themes',
+		'update_themes',
 	];
 
 	$plugin_caps = [
@@ -19,13 +23,19 @@ function mock_woa_free_plan_caps( $caps, $cap ) {
 		'install_plugins',
 		'edit_plugins',
 		'upload_plugins',
+		'update_plugins',
 	];
 
-	$all_atomic_caps = array_merge( $theme_caps, $plugin_caps );
+	$core_caps = [
+		'update_core',
+		'update_languages',
+	];
+
+	$all_atomic_caps = array_merge( $theme_caps, $plugin_caps, $core_caps );
 
 	if ( in_array( $cap, $all_atomic_caps, true ) ) {
 		$caps[] = 'do_not_allow';
-  }
+	}
 
 	return $caps;
 }
